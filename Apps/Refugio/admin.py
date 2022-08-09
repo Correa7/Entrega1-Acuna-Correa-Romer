7bf61c11cb3f44eb7ad451d29aed949a2d05af24
KedyAcu22 <110190819+KedyAcu22@users.noreply.github.com>
2022-08-09 19:14:54 -0300

    dir

--- Apps/Refugio/admin.py
+++ Apps/Refugio/admin.py
@@ -3,6 +3,6 @@ from Apps.Refugio.models import Refugio
 # Register your models here.
 
 class Refugio_admin(admin.ModelAdmin):
-    list_display = ["nombre","telefono","email","direccion"]
+    list_display = ["nombre","telefono","email","domicilio"]
 
 admin.site.register(Refugio, Refugio_admin)
