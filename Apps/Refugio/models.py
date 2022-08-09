a51dde336206cbb7e429a7c55109db4e7e599531
KedyAcu22 <110190819+KedyAcu22@users.noreply.github.com>
2022-08-09 18:34:49 -0300

    modesl

--- Apps/Refugio/models.py
+++ Apps/Refugio/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 class Refugio(models.Model):
 
-    nombre = models.CharField(max_length= 60, unique=True)
+    nombre = models.CharField(max_length= 80, unique=True)
     telefono = models.IntegerField(unique=True)
     email = models.EmailField(unique=True)
-    direccion = models.TextField(max_length=120) 
+    direccion = models.TextField(max_length=200) 
    
 
 
     def __str__(self):
         return f" {self.nombre} - {self.telefono} - {self.email} - {self.direccion}"
-
