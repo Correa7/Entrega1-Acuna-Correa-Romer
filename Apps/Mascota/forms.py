bc1e1c11904b904576715cf2e59fff3e1e97ce97
augusto romer <caugusto.romer@gmail.com>
2022-08-09 15:22:05 -0300

    cambios html titulo y placeholder

--- Apps/Mascota/forms.py
+++ Apps/Mascota/forms.py
@@ -1,4 +1,5 @@
 from contextlib import nullcontext
+from logging import PlaceHolder
 from django import forms
 
 #Variables para los ChoiceField
@@ -6,10 +7,10 @@ sexos = [(' Macho',' Macho'),(' Hembra',' Hembra')]
 lista_especies = [(' Perro',' Perro'),(' Gato',' Gato')]
 
 class Mascota_form (forms.Form):
-    nickname = forms.CharField()
+    nickname = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Ingrese texto'}))
     especie = forms.ChoiceField(choices=lista_especies,required=True,label='Seleccione la especie')
-    raza = forms.CharField()
+    raza = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Ingrese texto'}))
     sexo =  forms.ChoiceField(choices=sexos,required=True,label='Seleccione el sexo')
-    edad_aprox= forms.IntegerField(label='Edad aproximada')
-    ingreso = forms.DateField()
-    observaciones = forms.CharField()
+    edad_aprox= forms.IntegerField(label='Edad aproximada',widget=forms.TextInput(attrs={'placeholder':'Ingrese número entero'}))
+    ingreso = forms.DateField(widget=forms.TextInput(attrs={'placeholder':'Formato AAAA-MM-DD'}))
+    observaciones = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Ingrese texto'}))
--- Apps/Veterinaria/forms.py
+++ Apps/Veterinaria/forms.py
@@ -10,11 +10,11 @@ Castrado = [(' Si',' Si'),(' No',' No')]
 
 class Ficha_form (forms.Form):
     
-    registro = forms.IntegerField(label='Registro numero:')
+    registro = forms.IntegerField(label='Registro numero:',widget=forms.TextInput(attrs={'placeholder':'Ingrese número entero'}))
     vacuna_1 = forms.ChoiceField(choices=vac1,required=True,label='Vacuna numero 1')
     vacuna_2 = forms.ChoiceField(choices=vac1,required=True,label='Vacuna numero 2')
     desparasitacion = forms.ChoiceField(choices=Desparasitado,required=True,label='Desparasitado')
     castracion = forms.ChoiceField(choices=Castrado,required=True,label='Castrado')
-    observaciones = forms.CharField(max_length= 200)
+    observaciones = forms.CharField(max_length= 200,widget=forms.TextInput(attrs={'placeholder':'Ingrese texto'}))
 
 
