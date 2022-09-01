3e108186a0453b436a9b0267028a01116d13e2b8
augusto romer <caugusto.romer@gmail.com>
2022-08-31 21:38:14 -0300

    App Veterinaria - Ficha, urls, views, forms, model

--- Apps/Veterinaria/views.py
+++ Apps/Veterinaria/views.py
@@ -4,50 +4,75 @@ import datetime
 from django.template import loader
 from Apps.Veterinaria.models import Ficha_medica
 from Apps.Veterinaria.forms import Ficha_form
+from django.views.generic import  DeleteView
+from django.views.generic.edit import UpdateView
+from Apps.Mascota.models import Mascota
 
-# Create your views here.
 
-
-def Ficha_veterinaria (request):
+def Ficha_veterinaria (request, pk):  
+    mascota= Mascota.objects.get(id=pk)
+    print(mascota)
+    print( "---------------debugueando-------------")
 
     if request.method == "POST":
+        form = Ficha_form(request.POST)
+        if form.is_valid():
+            # data = form.cleaned_data
+            # ficha = Ficha_medica(
+                                
+            #                     registro=data["registro"],
+            #                     vacuna_1=data["vacuna_1"],
+            #                     vacuna_2=data["vacuna_2"],
+            #                     desparasitacion=data["desparasitacion"],
+            #                     castracion=data["castracion"],
+            #                     observaciones=["observaciones"],
+            #                  )
 
-        miFormulario = Ficha_form(request.POST)
+            Ficha_medica.objects.create( 
+                mascota= Mascota.objects.get(id=pk),
+                registro = form.cleaned_data['registro'],
+                vacuna_1 = form.cleaned_data['vacuna_1'],
+                vacuna_2 = form.cleaned_data['vacuna_2'],
+                desparasitacion = form.cleaned_data['desparasitacion'],
+                castracion = form.cleaned_data['castracion'],
+                observaciones = form.cleaned_data['observaciones'],
+            )
 
-        if miFormulario.is_valid():
 
-            data = miFormulario.cleaned_data
-
-            ficha = Ficha_medica(registro = data["registro"], vacuna_1 = data["vacuna_1"], vacuna_2 = data["vacuna_2"], desparasitacion= data["desparasitacion"], castracion= data["castracion"], observaciones= data["observaciones"])
-
-            ficha.save()
-
-            return render (request, "inicio.html" , context ={})
-
+            return redirect ( "lista-mascota")
     else:
 
-        miFormulario = Ficha_form()    
-
-    return render(request, "Veterinaria/ficha_veterinaria.html", {"miFormulario" : miFormulario})
-
+        form = Ficha_form()    
+    return render(request, "Veterinaria/ficha_veterinaria.html", {"form" : form})
 
 
 def busqueda_Vacuna (request):
-
     return render (request, "Veterinaria/ficha_busqueda_veterinaria.html")
 
 
 def buscar_ficha (request):
-
     if request.GET["registro"]:
-
         registro= request.GET["registro"]
         fichas= Ficha_medica.objects.filter(registro__icontains = registro)
-
         return render(request, "Veterinaria/ficha_resultado_veterinaria.html", {"fichas": fichas,"registro": registro})
-
     else:
-
         respuesta ="No enviaste datos"
-
     return render (request, "inicio.html", {"respuesta": respuesta})
+
+
+def lista_fichas (request):
+    lista = Ficha_medica.objects.all()
+    return render (request, "Veterinaria/lista_Ficha.html", {"lista": lista})
+
+
+class Borrar_Vacuna(DeleteView):
+    model = Ficha_medica
+    template_name= 'Veterinaria/borrar_vacuna.html'
+    success_url = '/Mascota/lista-mascota/'
+
+
+class Editar_Vacuna(UpdateView):
+    model = Ficha_medica
+    template_name = 'Veterinaria/editar_vacuna.html'
+    fields = ["vacuna_1","vacuna_2","desparasitacion","castracion","observaciones"]
+    success_url = '/Mascota/lista-mascota/'
