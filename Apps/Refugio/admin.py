73bd37800e47d78240f0762d14c4d2572cf95006
Correa7 <correadamian2019@gmail.com>
2022-09-03 14:32:08 -0300

    Readme link a pagina y video

--- Apps/Refugio/admin.py
+++ Apps/Refugio/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from Apps.Refugio.models import Refugio
-# Register your models here.
+from Apps.Refugio.models import Refugio, Perfil_Refugio
 
 class Refugio_admin(admin.ModelAdmin):
-    list_display = ["nombre","telefono","email","direccion"]
+    list_display = ["nombre","telefono","email","domicilio"]
 
-admin.site.register(Refugio, Refugio_admin)   
+admin.site.register(Refugio, Refugio_admin)
+
+
+class Perfil_Refugio_admin(admin.ModelAdmin):
+    list_display = ["id","image_1","image_2"]
+
+admin.site.register(Perfil_Refugio, Perfil_Refugio_admin)   
